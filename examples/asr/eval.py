c3e7c0b916c95585ef2288a11fde53724e1fe8a0
bofenghuang <bofenghuang7@gmail.com>
2023-10-03 18:46:28 +0200

    Update ASR eval

--- examples/asr/eval.py
+++ examples/asr/eval.py
@@ -12,33 +12,52 @@ from pathlib import Path
 import fire
 import pandas as pd
 
-# from myscripts.text.normalize_french import FrenchTextNormalizer
-from myscripts.text.normalize_french_zaion import FrenchTextNormalizer
-from myscripts.text.compute_wer import compute_wer
+# NB
+from text_normalization.normalize_french import FrenchTextNormalizer
+# from text_normalization.normalize_french_zaion import FrenchTextNormalizer
+from asr_metric_calculation.compute_wer import compute_wer
 
 
-def main(dataset_manifest_path, out_dir=None, do_ignore_words=False):
+def main(
+    dataset_manifest_path,
+    out_dir=None,
+    audio_column_name="audio_filepath",
+    target_column_name="text",
+    prediction_column_name="pred_text",
+    id_column_name="ID",
+    do_lowercase=True,
+    do_ignore_words=False,
+    symbols_to_keep="'-",
+    do_standardize_numbers=True,
+):
     data = []
     with open(dataset_manifest_path, "r") as f:
         for line in f:
+            line = line.strip()
+            # bh: for dekuple
+            # line = re.sub(r"\bNaN\b", '"nan"', line)
             data.append(json.loads(line))
 
     result_df = pd.DataFrame(data)
-    result_df.rename(columns={"audio_filepath": "wav", "text": "wrd", "pred_text": "pred_wrd"}, inplace=True)
-    result_df["ID"] = result_df["wav"].map(lambda x: Path(x).stem)
-    print(result_df.head())
+    # result_df.rename(columns={"audio_filepath": "wav", "text": "wrd", "pred_text": "pred_wrd"}, inplace=True)
+    result_df[id_column_name] = result_df[audio_column_name].map(lambda x: Path(x).stem)
+    # print(result_df.head())
     print(f"#utterances: {result_df.shape[0]}")
 
     text_normalizer = FrenchTextNormalizer()
+    # text_normalizer = FrenchTextNormalizer(remove_diacritics=True)
 
-    target_column_name = "wrd"
-    prediction_column_name = "pred_wrd"
-    id_column_name = "ID"
+    # print(result_df[result_df[target_column_name].isna()])
+    # quit()
 
     def norm_func(s):
         # NB
         return text_normalizer(
-            s, do_lowercase=True, do_ignore_words=do_ignore_words, symbols_to_keep="'-", do_standardize_numbers=True
+            s,
+            do_lowercase=do_lowercase,
+            do_ignore_words=do_ignore_words,
+            symbols_to_keep=symbols_to_keep,
+            do_standardize_numbers=do_standardize_numbers,
         )
 
     result_df[target_column_name] = result_df[target_column_name].map(norm_func)
--- examples/asr/transcribe_speech.sh
+++ examples/asr/transcribe_speech.sh
@@ -3,47 +3,124 @@
 
 myscriptspath="/home/bhuang/myscripts"
 export PYTHONPATH="${PYTHONPATH:-}:$myscriptspath"
-export PYTHONUNBUFFERED=1
+export PYTHONUNBUFFERED="1"
 
-export HYDRA_FULL_ERROR=1
+export HYDRA_FULL_ERROR="1"
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES="0"
 
 NEMO_GIT_FOLDER="/home/bhuang/asr/NeMo"
 
-# modelpath="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_new/checkpoints/stt_fr_conformer_transducer_large.nemo"
-# modelpath="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/checkpoints/stt_fr_conformer_transducer_large.nemo"
-modelpath="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/checkpoints/stt_fr_conformer_transducer_large-averaged.nemo"
+infer_and_eval() {
+    modelpath=$1
+    testmanifest=$2
+    outdir=$3
+    decodeopts=$4
 
-# testmanifest="/home/bhuang/asr/NeMo/examples/asr/tmp/test_hmhm.json"
-# testmanifest="/home/bhuang/corpus/speech/internal/hm_hm_16k/manifest_nemo/test_hmhm.json"
-# testmanifest="/projects/corpus/voice/zaion/lbpa/2023-02-21/data/nemo_manifest/data_without_partial_words.json"
-testmanifest="/projects/corpus/voice/zaion/lbpa/2023-02-22/data/nemo_manifest/data_without_partial_words.json"
+    # echo $modelpath
+    # echo $testmanifest
+    # echo $outdir
+    # echo $decodeopts
 
-# audio_dir="" \
+    [ -d $outdir ] || mkdir -p $outdir
 
-# outdir="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/outputs_averaged/test_hmhm_greedy"
-# outdir="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/outputs_averaged/test_hmhm_beam10"
-# outdir="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/outputs_averaged/test_lbpa_greedy"
-outdir="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/outputs_averaged/test_lbpa_lot2_beam10"
+    # pretrained_name="nvidia/stt_fr_fastconformer_hybrid_large_pc"
+    # audio_dir
+    # decoder_type
 
-decode_opts=(rnnt_decoding.strategy="beam" rnnt_decoding.beam.beam_size=10)
-# rnnt_decoding.strategy="beam" \
-# rnnt_decoding.beam.beam_size=10 \
+    # model_path=$modelpath \
+    # pretrained_name=$modelpath \
 
-python ${NEMO_GIT_FOLDER}/examples/asr/transcribe_speech.py \
-    model_path=$modelpath \
-    dataset_manifest=$testmanifest \
-    output_filename="$outdir/predictions.json" \
-    batch_size=16 \
-    num_workers=4 \
-    compute_langs=False \
-    cuda=0 \
-    amp=True \
-    ${decode_opts[@]} \
-    append_pred=False \
-    pred_name_postfix=""
+    # infer
+    python ${NEMO_GIT_FOLDER}/examples/asr/transcribe_speech.py \
+        pretrained_name=$modelpath \
+        dataset_manifest=$testmanifest \
+        output_filename="$outdir/predictions.json" \
+        batch_size=1 \
+        num_workers=4 \
+        append_pred=False \
+        pred_name_postfix="" \
+        compute_langs=False \
+        cuda=0 \
+        amp=True \
+        ${decodeopts[@]}
 
-# eval predictions
-python ${NEMO_GIT_FOLDER}/examples/asr/eval.py --dataset_manifest_path "$outdir/predictions.json" --out_dir $outdir --do_ignore_words False
-python ${NEMO_GIT_FOLDER}/examples/asr/eval.py --dataset_manifest_path "$outdir/predictions.json" --out_dir $outdir --do_ignore_words True
+    # eval predictions
+    python ${NEMO_GIT_FOLDER}/examples/asr/eval.py --dataset_manifest_path "$outdir/predictions.json" --out_dir $outdir --do_ignore_words False
+    # python ${NEMO_GIT_FOLDER}/examples/asr/eval.py --dataset_manifest_path "$outdir/predictions.json" --out_dir $outdir_ --do_ignore_words True
+}
+
+# decoding options
+# decode_opts=(rnnt_decoding.strategy="beam" rnnt_decoding.beam.beam_size=10)
+decode_opts=(decoder_type="ctc")
+
+# model
+# nvidia
+# model_path="nvidia/stt_fr_conformer_ctc_large"
+model_path="nvidia/stt_fr_conformer_transducer_large"
+# model_path="nvidia/stt_fr_fastconformer_hybrid_large_pc"
+# general
+# model_path="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_fastconformer_hybrid_transducer_ctc_bpe/large_bs2048_lr1e3/stt_fr_fastconformer_hybrid_transducer_ctc_bpe_large/2023-09-18_20-33-08/checkpoints/stt_fr_fastconformer_hybrid_transducer_ctc_bpe_large.nemo"
+# model_path="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_fastconformer_hybrid_transducer_ctc_bpe/large_bs2048_lr1e3/stt_fr_fastconformer_hybrid_transducer_ctc_bpe_large/2023-09-18_20-33-08/checkpoints/stt_fr_fastconformer_hybrid_transducer_ctc_bpe_large-averaged.nemo"
+# zaion hmhm
+# model_path="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_new/checkpoints/stt_fr_conformer_transducer_large.nemo"
+# model_path="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/checkpoints/stt_fr_conformer_transducer_large.nemo"
+# model_path="/home/bhuang/asr/NeMo/examples/asr/nemo_experiments/stt_fr_conformer_transducer_large/hmhm_merged_and_raw_ft_pretrained_bpe/checkpoints/stt_fr_conformer_transducer_large-averaged.nemo"
+
+# root_outdir="nemo_experiments/pretrained_stt_fr_conformer_ctc_large/outputs"
+root_outdir="nemo_experiments/pretrained_stt_fr_conformer_transducer_large/outputs"
+# root_outdir="nemo_experiments/pretrained_stt_fr_fastconformer_hybrid_large_pc/outputs"
+# root_outdir="nemo_experiments/stt_fr_fastconformer_hybrid_transducer_ctc_bpe/large_bs2048_lr1e3/stt_fr_fastconformer_hybrid_transducer_ctc_bpe_large/2023-09-18_20-33-08/outputs_averaged"
+
+# general data
+########################################################################################################################################################################################################
+# test_manifest="/projects/bhuang/corpus/speech/nemo_manifests/mozilla-foundation/common_voice_13_0/fr/test/test_mozilla-foundation_common_voice_13_0_manifest_normalized.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/mcv_test_greedy"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/mcv_test_ctc_greedy" $decode_opts
+
+# test_manifest="/projects/bhuang/corpus/speech/nemo_manifests/facebook/multilingual_librispeech/french/test/test_facebook_multilingual_librispeech_manifest_normalized.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/mls_test_greedy"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/mls_test_ctc_greedy" $decode_opts
+
+# test_manifest="/projects/bhuang/corpus/speech/nemo_manifests/facebook/voxpopuli/fr/test/test_facebook_voxpopuli_manifest_normalized.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/voxpopuli_test_greedy"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/voxpopuli_test_ctc_greedy" $decode_opts
+
+# test_manifest="/projects/bhuang/corpus/speech/nemo_manifests/google/fleurs/fr_fr/test/test_google_fleurs_manifest_normalized.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/fleurs_test_greedy"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/fleurs_test_ctc_greedy" $decode_opts
+
+# test_manifest="/projects/bhuang/corpus/speech/nemo_manifests/mtedx/fr-fr/test/test_mtedx_manifest_normalized.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/mtedx_test_greedy"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/mtedx_test_ctc_greedy" $decode_opts
+
+# test_manifest="/projects/bhuang/corpus/speech/nemo_manifests/gigant/african_accented_french/test/test_gigant_african_accented_french_manifest_normalized.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/african_accented_french_test_greedy"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/african_accented_french_test_ctc_greedy" $decode_opts
+
+test_manifest="/projects/bhuang/corpus/speech/nemo_manifests/speech-recognition-community-v2/dev_data/fr/validation/validation_speech-recognition-community-v2_dev_data_manifest_normalized.json"
+infer_and_eval $model_path $test_manifest "${root_outdir}/speech_recognition_community_v2_dev_data_greedy"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/speech_recognition_community_v2_dev_data_ctc_greedy" $decode_opts
+
+# zaion data
+########################################################################################################################################################################################################
+
+# test_manifest="/home/ywang/NeMo/examples/asr_zaion/190h_manifest/test_manifest_segment_16k_190h.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_hmhm_greedy"
+# # infer_and_eval $model_path $test_manifest "${root_outdir}/test_hmhm_beam10" $decode_opts
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_hmhm_ctc_greedy" $decode_opts
+
+# test_manifest="/home/ywang/NeMo/examples/asr_zaion/benchmark_robustess/carglass_5h/16k_segment.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_carglass_greedy"
+# # # infer_and_eval $model_path $test_manifest "${root_outdir}/test_carglass_beam10" $decode_opts
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_carglass_ctc_greedy" $decode_opts
+
+# test_manifest="/home/ywang/NeMo/examples/asr_zaion/benchmark_robustess/dekuple_5h/16k_segment.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_dekuple_greedy"
+# # # infer_and_eval $model_path $test_manifest "${root_outdir}/test_dekuple_beam10" $decode_opts
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_dekuple_ctc_greedy" $decode_opts
+
+# test_manifest="/home/ywang/NeMo/examples/asr_zaion/benchmark_robustess/lbpa_2.35h/16k_segment.json"
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_lbpa_greedy"
+# # # infer_and_eval $model_path $test_manifest "${root_outdir}/test_lbpa_beam10" $decode_opts
+# infer_and_eval $model_path $test_manifest "${root_outdir}/test_lbpa_ctc_greedy" $decode_opts
